7acd28f4d8af7752924f7f675997745b088b3729
Glitch (jsanderson-voting) <none>
2018-06-01 13:21:30 +0000

    set up delete routes for polls

--- public/poll-edit.js
+++ public/poll-edit.js
@@ -1,18 +1,25 @@
 //options for editing a poll
-/*
+
 $(document).ready(function() {
   
-  $('#deletepoll').click(function(e) {
+  $('.deletepoll').click(function(e) {
     
     if(confirm("This will delete your poll! Are you sure you want to do that?")) {
       
       var pollId = $(this).attr("id");
       
       $.ajax({
-        type: "GET",
-        url: "/poll/delete",
+        type: "DELETE",
+        url: "/poll/" + pollId,
         data: pollId,
-        success: function() {console.log("Deleting poll request...")}
+        dataType: "text",
+        success: function(data) {
+          window.location.href="/poll";
+        },
+        error: function(data) {
+          //to look at: why does an error occur?
+          window.location.href="/poll";
+        }
       });
       
     }
@@ -20,4 +27,3 @@ $(document).ready(function() {
   });
   
 });
-*/
